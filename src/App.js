f9a8bcd9f5e50a1b4f0397941e56007035854db4
marekwino23 <marekw1996@gmail.com>
2022-06-14 14:37:52 +0200

    setup components and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
-import { Routes, Link, Navigate, Route, useNavigate } from "react-router-dom";
+import { Routes, Link, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import Contact from "./pages/Contact";
 import { makeAutoObservable } from "mobx";
 import { observer } from "mobx-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>My project</h1>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/details">Details</Link>
+          <Link to="/contact">Contact</Link>
+        </ul>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
